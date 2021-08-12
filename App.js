8b1b8197cc90e69958ed0ec088d657e697dcd526
RohanK22 <rohank2205@gmail.com>
2021-08-12 19:58:18 +0530

    added wireframes ;

--- App.js
+++ App.js
@@ -1,111 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
+import Todo from './components/Todo';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Alert,
   StyleSheet,
+  Image,
   Text,
-  useColorScheme,
   View,
+  ScrollView,
+  SafeAreaView,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = () => {
+  const [todos, setTodos] = useState([<Todo key="alskjdf" />]);
+  
+  function addNewTodo() {
+    todos.push(<Todo key={todos.length} text="test" />);  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.root}>
+      <Button onPress={addNewTodo} title='+' style={styles.button}></Button>
+      {todos.forEach((todo) => {
+        return (
+          todo
+        );
+      })}
     </SafeAreaView>
   );
-};
+};  
+
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  root: {
+    backgroundColor: '#f0ffff',
+    flex:1
+  },  
+  button: {
+    position: 'absolute',
+    top: 50,
+    marginTop: 100, 
+    bottom: 0,
+    right: 0,
+  },  
+  text: {
+    fontSize: 25,    
   },
 });
 
---
+++ components/Todo.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import {
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+
+const Todo = (props) => {
+  const [text, setText] = useState(props.text ? props.text : 'test');
+
+  return (
+      <Text style={styles.text}>test</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25,    
+  },
+});
+
+
+export default Todo;
