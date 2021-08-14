765250f1abda1954b69fe80e0285c0ce5f67958a
RohanK22 <rohank2205@gmail.com>
2021-08-14 13:40:19 +0530

    Added + feature in list

--- components/List.js
+++ components/List.js
@@ -1,23 +1,50 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {Alert, ScrollView, TouchableOpacity, Text, StyleSheet, View, TextInput} from 'react-native';
 import Todo from './Todo';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 class List extends Component {
-    state = {
-        todos: [
-            {
-                text: 'Wash dishes',
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {
+                    text: 'Wash dishes',
+                    isCompleted: false,
+                },
+                {
+                    text: 'Take out trash',
+                    isCompleted: false,
+                },
+                {
+                    text: 'Get Veggies',
+                    isCompleted: false,
+                }
+            ],
+            textInputValue: '',
+        };
+        this.onChangeInput = this.onChangeInput.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    onChangeInput(text) {
+        this.setState({
+            textInputValue: text
+        });
+    }
+
+    addTodo() {
+        if(this.state.textInputValue.length != 0) {
+            this.state.todos.push({
+                text: this.state.textInputValue,
                 isCompleted: false,
-            },
-            {
-                text: 'Take out trash',
-                isCompleted: false,
-            },
-            {
-                text: 'Get Veggies',
-                isCompleted: false,
-            }
-        ]
+            });
+            console.log(this.state.todos);
+            this.setState({
+                textInputValue: ''
+            });
+        }
     }
     
     render(){
@@ -26,11 +53,40 @@ class List extends Component {
         });
 
         return (
-            <View>
-                {todoComponents}
-            </View>
+            <ScrollView>
+                <View style={{flex: 1, padding: 10, flexDirection: 'row'}}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Todo..."
+                        backgroundColor="black"
+                        onChangeText={text => this.onChangeInput(text)}
+                        value={this.state.textInputValue}
+                    />
+                    <TouchableOpacity style={styles.addTodoButton} onPress={this.addTodo}><Icon name="plus" size={30} /></TouchableOpacity>
+                </View>
+                <ScrollView style={styles.listView}>
+                    {todoComponents}
+                </ScrollView>
+            </ScrollView>
         );        
     }
 }
+
+const styles = StyleSheet.create({
+    listView: {
+        flex: 1,
+    },
+    input: {
+      flex: 6,
+      borderRadius: 40,  
+    },
+    addTodoButton: {
+        alignItems: 'center',
+        alignContent: 'center',
+        flex: 1,
+        backgroundColor: "green",
+        borderRadius: 40,
+    }
+});
 
 export default List;
